97bdbc28c9a9418c55216d6adfdf0752d59d8a07
Sandro <santanaribeiro2002@gmail.com>
2022-10-21 19:39:57 -0300

    Administrador

---
+++ Model/administrador.py
@@ -0,0 +1,29 @@
+class Administrador:
+    def __int__(self, nome, codigo, senha):
+        self.__nome = nome
+        self.__codigo = codigo
+        self.__senha = senha
+
+    @property
+    def nome(self):
+        return self.__nome
+
+    @property
+    def codigo(self):
+        return self.__codigo
+
+    @property
+    def senha(self):
+        return self.__senha
+
+    @nome.setter
+    def nome(self, nome):
+        self.__nome = nome
+
+    @codigo.setter
+    def codigo(self, codigo):
+        self.__codigo = codigo
+
+    @senha.setter
+    def senha(self, senha):
+        self.__senha = senha
