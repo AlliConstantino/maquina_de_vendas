6c572df573b1ee74dbdbab3f28a7557c207de584
Alícia Constantino Costa <aliciaconstantino.costa@gmail.com>
2022-10-25 18:54:27 -0300

    Create bebida.py
    variavel 'tipo" adicionada, assim como os getters e setters, para que depois seja possivel usa-la e definir se um produto é bebida ou comida dentro da maquina

---
+++ Model/bebida.py
@@ -0,0 +1,50 @@
+class Produto:
+    def __int__(self,nome: str, codigo, preco: float , quantidade: int, tipo: str):
+        self.__nome = nome
+        self.__codigo = codigo
+        self.__quantidade = quantidade
+        self.__tipo = tipo
+
+
+
+        @property
+        def tipo(self):
+            return self.__tipo
+
+        @property
+        def nome(self):
+            return self.__nome
+
+        @property
+        def codigo(self):
+            return self.__codigo
+
+        @property
+        def preco(self):
+            return self.__preco
+
+        @property
+        def quantidade(self):
+            return self.__quantidade
+
+        #setters
+
+        @nome.setter
+        def nome(self, nome:str):
+            self.__nome = nome
+
+        @codigo.setter
+        def codigo(self, codigo):
+            self.__codigo = codigo
+
+        @preco.setter
+        def preco(self, preco: float):
+            self.__preco = preco
+
+        @quantidade.setter
+        def quantidade(self, quantidade: int):
+            self.__quantidade = quantidade
+
+        @tipo.setter
+        def tipo(self, tipo: str):
+            self.__tipo = tipo
