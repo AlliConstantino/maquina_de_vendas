115d533a50ea1e3f91cb57073b03b9a689f830a4
Alícia Constantino Costa <aliciaconstantino.costa@gmail.com>
2022-10-26 23:50:43 -0300

    Adicionando importação do controlador_maquina e uma tentativa de ajuste no main

--- main.py
+++ main.py
@@ -2,8 +2,9 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from Controller.controlador_maquina import ControladorMaquina
 
-
+#tirar esse primeiro def?
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -11,6 +12,6 @@ def print_hi(name):
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    Controller.controlador_maquina.iniciar() #criar esse metodo depois
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
